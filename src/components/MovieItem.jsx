000ab5435d2093279bb40edcaffc63ceb563f5a8
onair-lena <pechenko@bk.ru>
2020-04-05 23:12:14 +0300

    willWatch button state added

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,8 +1,15 @@
 import React from "react"
 
-const MovieItem = (props) => {
-  console.log(props)
-  const {movie, removeMovie, addMovieToWillWatch} = props
+class MovieItem extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      willWatch: false
+     }
+  }
+  render() {
+    console.log(this.props)
+  const {movie, removeMovie, addMovieToWillWatch} = this.props
   return (
     <div className="card">
       <img className="card-img-top"
@@ -14,14 +21,53 @@ const MovieItem = (props) => {
       <h6 className="card-title">{movie.title}</h6>
         <div className="d-flex justify-content-between align-items-center">
           <p className="mb-0">Rating: {movie.vote_average}</p>
-          <button type="button" className="btn btn-secondary" onClick={addMovieToWillWatch.bind(null, movie)}>
-            Will Watch
+          {this.state.willWatch ? 
+            <button 
+              type="button"
+              className="btn btn-success"
+            >
+              Remove Will Watch
+            </button>
+          :
+          <button 
+            type="button"
+            className="btn btn-secondary"
+            onClick={addMovieToWillWatch.bind(null, movie)}
+          >
+            Add Will Watch
           </button>
+          }
+
         </div>
         <button onClick={ removeMovie.bind(null, movie) }>Delete Movie</button>
       </div>
     </div>
   )
+  }
 }
+
+// const MovieItem = (props) => {
+//   console.log(props)
+//   const {movie, removeMovie, addMovieToWillWatch} = props
+//   return (
+//     <div className="card">
+//       <img className="card-img-top"
+//       src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path ||
+//         movie.poster_path}`}
+//       alt=""
+//     />
+//     <div className="card-body">
+//       <h6 className="card-title">{movie.title}</h6>
+//         <div className="d-flex justify-content-between align-items-center">
+//           <p className="mb-0">Rating: {movie.vote_average}</p>
+//           <button type="button" className="btn btn-secondary" onClick={addMovieToWillWatch.bind(null, movie)}>
+//             Will Watch
+//           </button>
+//         </div>
+//         <button onClick={ removeMovie.bind(null, movie) }>Delete Movie</button>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default MovieItem
